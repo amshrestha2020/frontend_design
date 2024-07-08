6b28298233464d5e39205216ec085c213fa6815c
Ashesh Shrestha <63858401+amshrestha2020@users.noreply.github.com>
2024-07-08 11:37:02 -0400

    Create Layout.jsx

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,20 @@
+// Layout.jsx
+import React from 'react';
+import Security from '../components/security/Security';
+import Sidebar from '../components/sidebar/Sidebar';
+import { Box } from '@mui/material';
+
+const Layout = ({ children }) => {
+  return (
+    <Box display="flex">
+      <Box flex="1">
+        <Security />
+        <Box p={3}>
+          {children}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
