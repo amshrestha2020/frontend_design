1686c1ea078b9a866b5e3a1848bec107998153e4
Ashesh Shrestha <asheshmshrestha@gmail.com>
2024-07-24 18:51:09 -0400

    minor changes

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Layout.jsx
 import React from 'react';
 import Security from '../components/security/Security';
@@ -17,25 +16,3 @@ const Layout = ({ children }) => {
 };
 
 export default Layout;
-=======
-// Layout.jsx
-import React from 'react';
-import Security from '../components/security/Security';
-import Sidebar from '../components/sidebar/Sidebar';
-import { Box } from '@mui/material';
-
-const Layout = ({ children }) => {
-  return (
-    <Box display="flex">
-      <Box flex="1">
-        <Security />
-        <Box p={3}>
-          {children}
-        </Box>
-      </Box>
-    </Box>
-  );
-};
-
-export default Layout;
->>>>>>> upstream/main
